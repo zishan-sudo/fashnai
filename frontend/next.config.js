f9990b82fc11747ed181657e59f3a21695e2196f
zishan-sudo <zishanahmed.ai@gmail.com>
2026-02-08 20:13:32 +0100

    Configure Next.js for static export on Netlify

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   trailingSlash: true,
+  distDir: 'out',
   images: {
     unoptimized: true,
     domains: ['localhost'],
